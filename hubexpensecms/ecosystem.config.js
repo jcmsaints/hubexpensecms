6f59a5b922367eb4382c0547174b5710e216c2c9
jcmsaints <joao.c.matias.santos@gmail.com>
2020-01-20 19:20:20 +0000

    environment server js

--- hubexpensecms/ecosystem.config.js
+++ hubexpensecms/ecosystem.config.js
@@ -1,10 +1,18 @@
 module.exports = {
   apps: [
     {
-      name: "API",
-      script: "app.js",
-      cwd: "",
-      env: {}
+      name: "strapi",
+      cwd: "/home/ubuntu/hubexpensecms",
+      script: "server.js",
+      env: {
+        NODE_ENV: "production",
+        DATABASE_HOST:
+          "strapi-database.cizkqgfuujoz.eu-west-2.rds.amazonaws.com", // database Endpoint under 'Connectivity & Security' tab
+        DATABASE_PORT: "5432",
+        DATABASE_NAME: "strapi", // DB name under 'Configuration' tab
+        DATABASE_USERNAME: "HubExpense", // default username
+        DATABASE_PASSWORD: "Hub$pense.1"
+      }
     }
   ]
 };
